7d96a5f094896b167a45389fc8a43f01f340d6e1
Arman Shah <armanshah360@gmail.com>
2018-03-07 01:04:28 -0800

    comments

--- ChocolateBarRatings/server.R
+++ ChocolateBarRatings/server.R
@@ -12,9 +12,9 @@ library(ggplot2)
 library(dplyr)
 
 #setwd("~/Desktop/Chocolate-Bar-Ratings")
-chocolate <- read.csv("./flavors_of_cacao.csv", header = TRUE, stringsAsFactors = FALSE)
+chocolate <- read.csv("./flavors_of_cacao.csv", header = TRUE, stringsAsFactors = FALSE, fileEncoding="UTF-8")
 
-# Define server logic required to draw a histogram and table 
+# Define server logic required to draw table 
 shinyServer(function(input, output) {
   output$table <- renderDataTable({
     data <- chocolate
@@ -32,10 +32,10 @@ shinyServer(function(input, output) {
     b1 <- n >= input$cocao[1] 
     b2 <- n <= input$cocao[2]
     data <- filter(data, b1 & b2)
-    if (input$beanorigin != "NONE") {
+    if (input$beanorigin != "No Preference") {
       data <- filter(data, Broad.Bean.Origin == input$beanorigin)
     }
-    if (input$country != "NONE") {
+    if (input$country != "Any") {
       data <- filter(data, Company.Location == input$country)
     }
     data <- arrange(data, desc(Rating))
@@ -58,15 +58,15 @@ shinyServer(function(input, output) {
     # if else to select what variable is being chosen to be grouped by
     if(input$property == "Company") {
       #group by
-      relevant_data <- group_by(chocolate, Company...Maker.if.known.) 
+      relevant_data <- group_by(chocolate, Company..Maker.if.known.) 
       #summarise the rating
       rating_mean <- summarise(relevant_data, mean_rating = mean(Rating)) %>% arrange(desc(mean_rating))  
       #taking the top 10
       rating_mean <- head(rating_mean, 10) 
       #plot
-      ggplot(rating_mean, aes(Company...Maker.if.known., mean_rating)) + 
-        geom_col(aes(fill = Company...Maker.if.known.)) + 
-        geom_text(aes(Company...Maker.if.known., mean_rating, label=round(mean_rating, digits=2)), vjust = 2, color = "white") +
+      ggplot(rating_mean, aes(Company..Maker.if.known., mean_rating)) + 
+        geom_col(aes(fill = Company..Maker.if.known.)) + 
+        geom_text(aes(Company..Maker.if.known., mean_rating, label=round(mean_rating, digits=2)), vjust = 2, color = "white") +
         labs(title = paste("Top Ten Cacao by", input$property), x = input$property, y = "Rating") +
         theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
         theme(legend.position="none")
